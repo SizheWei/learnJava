6bff46b5f8e72faa5bc8ce7d933153ad1047ef7e
Sizhe Wei <sizhewei@sjtu.edu.cn>
2019-07-14 11:24:58 +0800

    Program 1.1.2

---
+++ UseArgument.java
@@ -0,0 +1,15 @@
+/*
+* Author: Sace
+* Data: Jul 14, 2019
+* Program 1.1.2
+*/
+
+public class UseArgument
+{
+    public static void main(String[] args)
+    {
+	System.out.print("Hi, ");
+	System.out.print(args[0]);
+	System.out.println(". How are you?");
+    }
+}
